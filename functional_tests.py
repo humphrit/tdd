2a5b8f9d12d5cb029e5691fef56c26c05fa389bc
humphrit8 <thom2@bgs.ac.uk>
2018-06-14 18:17:41 +0100

    Hopefully removed .idea

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -14,19 +16,32 @@ class NewVisitorTest(unittest.TestCase):
 
     # Page header and title mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
     # Enter to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item'
+        )
 
 
     # Type into text box
+        inputbox.send_keys('Buy peacock feathers')
 
 
     # Update the page - item added to to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Buy peacock feathers' for row in rows)
+        )
 
     # Add another item
-
+        self.fail('Finish the test')
 
     # Update again
 
